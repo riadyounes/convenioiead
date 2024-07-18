3dcfe9b7622ad2ee0ea05c130ab840f760b99113
Sandro Thomé <sandro.thome@grupobipp.com.br>
2024-07-18 18:25:52 -0300

    Ajustada rota de listagem de cupons do convênio.

--- src/app/api/convenios/[slug]/route.ts
+++ src/app/api/convenios/[slug]/route.ts
@@ -1,36 +1,37 @@
-import { PrismaClient } from "@prisma/client";
-import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from '@prisma/client'
+import { NextRequest, NextResponse } from 'next/server'
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
-export interface Params {
-  slug: string;
+export interface CupomParams {
+  slug: string
 }
 
-export const GET = async (req: NextRequest, { params }: { params: Params }) => {
-  const slug = params.slug;
+export const GET = async (req: NextRequest, { params }: { params: CupomParams }) => {
+  const slug = params.slug
 
   if (!slug) {
     return NextResponse.json(
       { error: 'Filtro de busca não informado' },
       { status: 400 },
-    );
+    )
   }
 
   const convenio = await prisma.covenant.findUnique({
     where: {
-      slug: slug,
+      slug,
     },
     include: {
       cupons: {
         select: {
           id: true,
+          date: true,
           amount: true,
           value: true,
         },
       },
     },
-  });
+  })
 
-  return NextResponse.json({ convenio });
-};
+  return NextResponse.json({ convenio })
+}
