ecaa595fd1326308686fc03066e47829f51181ba
Sandro Thomé <sandro.thome@grupobipp.com.br>
2024-07-18 18:37:21 -0300

    Ajustado componente para receber os cupons via props.

--- src/app/[slug]/table-cupons.tsx
+++ src/app/[slug]/table-cupons.tsx
@@ -10,58 +10,22 @@ import {
 } from '@/components/ui/table'
 import { format } from 'date-fns'
 
-const cupons = [
-  {
-    id: 1,
-    date: new Date().toISOString(),
-    amount: 10,
-    value: '250.00',
-  },
-  {
-    id: 2,
-    date: new Date().toISOString(),
-    amount: 10,
-    value: '250.00',
-  },
-  {
-    id: 3,
-    date: new Date().toISOString(),
-    amount: 10,
-    value: '350.05',
-  },
-  {
-    id: 4,
-    date: new Date().toISOString(),
-    amount: 10,
-    value: '450.50',
-  },
-  {
-    id: 5,
-    date: new Date().toISOString(),
-    amount: 10,
-    value: '550.00',
-  },
-  {
-    id: 6,
-    date: new Date().toISOString(),
-    amount: 10,
-    value: '200.00',
-  },
-  {
-    id: 7,
-    date: new Date().toISOString(),
-    amount: 20,
-    value: '300.00',
-  },
-]
+interface Cupom {
+  date: string
+  quantity: number
+  value: number
+}
 
-export function TableCupons() {
+interface TableCuponsProps {
+  cupons: Cupom[]
+}
+
+export function TableCupons({ cupons }: TableCuponsProps) {
   const totalValue = cupons.map((cupom) => Number(cupom.value))
 
-  const sumTotalValue = totalValue.reduce(
-    (previousValue, currentValue) => previousValue + currentValue,
-    0,
-  )
+  const sumTotalValue = totalValue
+    .reduce((previousValue, currentValue) => previousValue + currentValue, 0)
+    .toFixed(2)
 
   console.log(sumTotalValue)
 
