59f66bea98aff32797fdb45d0cc9f18dfd8c8587
Sandro Thomé <sandro.thome@grupobipp.com.br>
2024-07-19 13:14:22 -0300

    Adicionada rota de exclusão de cupons.

---
+++ src/app/api/cupons/[id]/delete/route.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from '@prisma/client'
+import { NextRequest, NextResponse } from 'next/server'
+
+const prisma = new PrismaClient()
+
+export const DELETE = async (
+  req: NextRequest,
+  { params }: { params: { id: string } },
+) => {
+  const cupomId = params.id
+
+  if (!cupomId) {
+    return NextResponse.json(
+      { error: 'É obrigatório informar o id do cupom' },
+      { status: 400 },
+    )
+  }
+
+  try {
+    await prisma.cupon.delete({
+      where: { id: cupomId },
+    })
+
+    return NextResponse.json({ message: 'Cupom excluído com sucesso.' })
+  } catch (error) {
+    return NextResponse.json(
+      { error: 'Erro ao excluir cupom.' },
+      { status: 500 },
+    )
+  }
+}
