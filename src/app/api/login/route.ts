24e4223749e23c651b40263c4cd4495135b5e3bb
Sandro Thomé <sandro.thome@grupobipp.com.br>
2024-08-14 18:32:42 -0300

    Added login route.

---
+++ src/app/api/login/route.ts
@@ -0,0 +1,59 @@
+import { PrismaClient } from '../../../../prisma/generated/client'
+import { NextRequest, NextResponse } from 'next/server'
+import bcrypt from 'bcrypt'
+
+const prisma = new PrismaClient()
+
+export const POST = async (req: NextRequest) => {
+  try {
+    const { covenantId, user_name, password } = await req.json()
+
+    if (!covenantId || !user_name || !password) {
+      return NextResponse.json(
+        { error: 'Preencha os campos obrigatórios' },
+        { status: 400 },
+      )
+    }
+
+    const user = await prisma.user.findUnique({
+      where: { userName: user_name },
+      select: {
+        id: true,
+        password: true,
+        role: true,
+        Covenant: {
+          select: {
+            id: true,
+          },
+        },
+      },
+    })
+
+    if (!user) {
+      return NextResponse.json(
+        { error: 'Credenciais inválidas' },
+        { status: 401 },
+      )
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+    const isCorrectUser = user.Covenant?.id === covenantId
+    const isAdminUser = user.role === 'ADMIN'
+
+    if (!isPasswordValid || (!isAdminUser && !isCorrectUser)) {
+      return NextResponse.json(
+        { error: 'Credenciais inválidas' },
+        { status: 401 },
+      )
+    }
+
+    // If all checks pass, user is authenticated
+    return NextResponse.json(
+      { message: 'Authenticated successfully' },
+      { status: 200 },
+    )
+  } catch (error) {
+    console.error(error)
+    return NextResponse.json({ error: 'An error occurred' }, { status: 500 })
+  }
+}
